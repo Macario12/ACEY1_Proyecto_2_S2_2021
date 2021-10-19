00a1f7e816ce3cb016595b80e991e3d210881ea9
Ariel Macario <arielmacario.11@gmail.com>
2021-10-18 20:33:50 -0600

    Archivo ASM

---
+++ pr3.asm
@@ -0,0 +1,12 @@
+include macros.asm ;archivo con los macros
+
+.model small
+;---------------------SEGMENTO DE PILA ----------------
+.stack
+;---------------------SEGMENTO DE DATO ----------------
+.data
+.code
+main proc
+
+main endp
+end main
